7f60b77e0ae511ca8ab87c34cd84711b937c9c2f
Aevismotax1 <robertz_chen@outlook.com>
2025-01-11 13:45:53 -0500

    adding several useful dependencies

---
+++ ChatbotCSharp/Service/ChatService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net.Http;
+
+
+namespace ChatbotCSharp.Service
+{
+    internal class ChatService
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+        private string key { get; set; }
+
+        private String ID {  get; set; }
+
+
+
+        public ChatService()
+        {
+            loadkey();
+        }
+
+
+        private void loadkey() 
+        {
+            try
+            {
+
+                var config = File.ReadAllText("secrets.json");
+                var text = JObject.Parse(config);
+                this.key = text["OpenAI_API_Key"].ToString();
+
+            } catch(Exception ex) { Console.WriteLine(ex.Message.ToString());  }
+
+        }
+
+
+
+
+        //If I wish to implement my own Chatbot
+
+    }
+}
